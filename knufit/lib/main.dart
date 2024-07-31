caa8d193d7f66be78f074f45eab7f8bb1b0a235d
Kms3506 <ixt@naver.com>
2024-07-31 17:58:47 +0900

    test

--- knufit/lib/main.dart
+++ knufit/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'signup/signup_page.dart';
 import 'login/login_page.dart';
-import 'home_screen.dart';
+//import 'home_screen.dart';
 
 void main() {
   runApp(FitnessApp());
