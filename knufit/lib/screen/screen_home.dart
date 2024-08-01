3835a347d93dee7533e176ee9c2bf14645435cb7
Kms3506 <ixt@naver.com>
2024-08-01 18:54:53 +0900

    appbar 개선

--- knufit/lib/screen/screen_home.dart
+++ knufit/lib/screen/screen_home.dart
@@ -26,29 +26,33 @@ class _ScreenHomeState extends State<ScreenHome> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('KNU Fit'),
-        //backgroundColor: Color.fromARGB(255, 163, 163, 163), // 투명한 파란색 배경
+        //backgroundColor: Color.fromARGB(255, 163, 163, 163), // 앱바 배경 색상 조정
         bottom: PreferredSize(
           preferredSize: Size.fromHeight(50.0),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              TextButton(
-                onPressed: () => _onTabSelected(0),
-                child: Text(
-                  'Home Memo',
-                  style: TextStyle(
-                    color: _selectedTabIndex == 0 ? Colors.blue : const Color.fromARGB(179, 41, 41, 41),
-                    fontWeight: _selectedTabIndex == 0 ? FontWeight.bold : FontWeight.normal,
+              Expanded(
+                child: TextButton(
+                  onPressed: () => _onTabSelected(0),
+                  child: Text(
+                    'Home Memo',
+                    style: TextStyle(
+                      color: _selectedTabIndex == 0 ? Colors.blue : const Color.fromARGB(179, 41, 41, 41),
+                      fontWeight: _selectedTabIndex == 0 ? FontWeight.bold : FontWeight.normal,
+                    ),
                   ),
                 ),
               ),
-              TextButton(
-                onPressed: () => _onTabSelected(1),
-                child: Text(
-                  'Routine',
-                  style: TextStyle(
-                    color: _selectedTabIndex == 1 ? Colors.blue : const Color.fromARGB(179, 41, 41, 41),
-                    fontWeight: _selectedTabIndex == 1 ? FontWeight.bold : FontWeight.normal,
+              Expanded(
+                child: TextButton(
+                  onPressed: () => _onTabSelected(1),
+                  child: Text(
+                    'Routine',
+                    style: TextStyle(
+                      color: _selectedTabIndex == 1 ? Colors.blue : const Color.fromARGB(179, 41, 41, 41),
+                      fontWeight: _selectedTabIndex == 1 ? FontWeight.bold : FontWeight.normal,
+                    ),
                   ),
                 ),
               ),
