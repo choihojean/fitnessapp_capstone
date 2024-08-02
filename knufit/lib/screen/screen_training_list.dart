57f889ca49ff705240941c69f00b45064ab5b6b4
Kms3506 <ixt@naver.com>
2024-08-02 23:14:29 +0900

    Revert "list +"
    This reverts commit 397c7c7a139209b22a9d6291d8edce8976e6d40e.

--- knufit/lib/screen/screen_training_list.dart
+++ knufit/lib/screen/screen_training_list.dart
@@ -1,82 +1,13 @@
 import 'package:flutter/material.dart';
 
-// Define a model class for Training
-class Training {
-  final String title;
-  final String description;
-  final String imageAsset;
-
-  Training({
-    required this.title,
-    required this.description,
-    required this.imageAsset,
-  });
-}
-
 class ScreenTrainingList extends StatelessWidget {
-  // Sample list of trainings
-  final List<Training> trainings = [
-    Training(
-      title: 'Training 1',
-      description: 'Description for training 1',
-      imageAsset: 'assets/test.png',
-    ),
-    Training(
-      title: 'Training 2',
-      description: 'Description for training 2',
-      imageAsset: 'assets/test.png',
-    ),
-    Training(
-      title: 'Training 3',
-      description: 'Description for training 3',
-      imageAsset: 'assets/test.png',
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Training List'),
+    return Center(
+      child: Text(
+        'Training List Screen',
+        style: TextStyle(fontSize: 30),
       ),
-      body: ListView(
-        children: trainings.map((training) {
-          return TrainingListTile(training: training);
-        }).toList(),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Handle the action when the + button is pressed
-          // Add functionality for adding new items or navigating to another screen
-        },
-        child: Icon(Icons.add),
-        tooltip: 'Add New Training',
-      ),
-    );
-  }
-}
-
-class TrainingListTile extends StatelessWidget {
-  final Training training;
-
-  TrainingListTile({required this.training});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: Image.asset(training.imageAsset),
-      title: Text(training.title),
-      subtitle: Text(training.description),
-      trailing: IconButton(
-        icon: Icon(Icons.add),
-        onPressed: () {
-          // Handle the action when the + icon is pressed
-          // Add functionality such as showing a dialog or performing an action
-        },
-      ),
-      onTap: () {
-        // Handle item tap, if necessary
-      },
     );
   }
 }
