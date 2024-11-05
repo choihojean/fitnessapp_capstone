5104c7ce05a312be839d3eed02311607b07be505
Ourumo <ss8638@naver.com>
2024-11-05 16:25:45 +0900

    feat: 내부db create와 update 분리 생성

--- knufit/lib/database/db_training.dart
+++ knufit/lib/database/db_training.dart
@@ -36,23 +36,8 @@ class DBTraining {
     );
   }
 
-  // 테이블 데이터 삭제
-  Future<Map<String, dynamic>> deleteTraining() async {
-    final db = await database;
-    Map<String, dynamic> result;
-    try {
-      await db.delete("training");
-      result = {"message": "success"};
-    } catch (e) {
-      result = {"message": "failure", "error": e};
-    } finally {
-      await db.close();
-    }
-    return result;
-  }
-  
-  // 테이블 데이터 추가
-  Future<Map<String, dynamic>> createAndUpdateTraining(Map<String, dynamic> data) async {
+  // 테이블 데이터 create => (데이터 리턴)
+  Future<Map<String, dynamic>> createTraining(Map<String, dynamic> data) async {
     final db = await database;
     Map<String, dynamic> result;
     try {
@@ -66,7 +51,7 @@ class DBTraining {
     return result;
   }
 
-  // 테이블 전체 데이터 로드
+  // 테이블 전체 데이터 read => (성공, 실패 여부 리턴)
   Future<List<Map<String, dynamic>>> readTrainingAll() async {
     final db = await database;
     List<Map<String, dynamic>> result = [];
@@ -83,7 +68,8 @@ class DBTraining {
     return result;
   }
 
-  // 테이블 전체 데이터 개수 로드
+
+  // 테이블 전체 데이터 개수 read => (int 리턴)
   Future<int> readTrainingAllCount() async {
     final db = await database;
     int result = 0;
@@ -98,7 +84,7 @@ class DBTraining {
     return result;
   }
 
-  // 테이블 단일 데이터 로드
+  // 테이블 단일 데이터 read => (데이터 리턴)
   Future<Map<String, dynamic>> readTraining(int trainingId) async {
     final db = await database;
     Map<String, dynamic>? result;
@@ -111,6 +97,36 @@ class DBTraining {
         limit: 1
       );
       result = res.isNotEmpty ? res.first : {"message": "failure"};
+    } catch (e) {
+      result = {"message": "failure", "error": e};
+    } finally {
+      await db.close();
+    }
+    return result;
+  }
+
+  // 테이블 데이터 update => (성공, 실패 여부 리턴)
+  Future<Map<String, dynamic>> updateTraining(Map<String, dynamic> data) async {
+    final db = await database;
+    Map<String, dynamic> result;
+    try {
+      await db.update("training", data, conflictAlgorithm: ConflictAlgorithm.replace);
+      result = {"message": "success"};
+    } catch (e) {
+      result = {"message": "failure", "error": e};
+    } finally {
+      await db.close();
+    }
+    return result;
+  }
+
+  // 테이블 데이터 delete => (성공, 실패 여부 리턴)
+  Future<Map<String, dynamic>> deleteTraining() async {
+    final db = await database;
+    Map<String, dynamic> result;
+    try {
+      await db.delete("training");
+      result = {"message": "success"};
     } catch (e) {
       result = {"message": "failure", "error": e};
     } finally {
