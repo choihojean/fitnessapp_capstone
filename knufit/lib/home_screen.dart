3e30f8d55a25e7d0ea5eb8811c0411edaf25b7c1
Kms3506 <ixt@naver.com>
2024-11-08 18:49:22 +0900

    프로필 페이지 로그인

--- knufit/lib/home_screen.dart
+++ knufit/lib/home_screen.dart
@@ -6,7 +6,7 @@ import 'screen/screen_calendar.dart';
 import 'screen/screen_menu.dart';
 
 class HomeScreen extends StatefulWidget {
-  final Map<String, dynamic> user; // 로그인 정보를 받을 변수
+  final Map<String, dynamic> user;
 
   const HomeScreen({required this.user, Key? key}) : super(key: key);
 
@@ -18,19 +18,10 @@ class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
   late Map<String, dynamic> user;
 
-  late List<Widget> _widgetOptions;
-
   @override
   void initState() {
     super.initState();
     user = widget.user;
-    _widgetOptions = <Widget>[
-      ScreenHome(user: user), // 로그인 정보를 전달
-      WorkoutScreen(),
-      ScreenTrainingList(user: user),
-      ScreenCalendar(user: user),
-      ScreenMenu(user: user), // 로그인 정보를 전달
-    ];
   }
 
   void _onItemTapped(int index) {
@@ -42,39 +33,27 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('안녕하세요, ${user['name']}님'), // 로그인한 사용자의 이름 표시
-      // ),
       body: IndexedStack(
         index: _selectedIndex,
-        children: _widgetOptions,
+        children: [
+          ScreenHome(user: user),
+          WorkoutScreen(),
+          ScreenTrainingList(user: user),
+          ScreenCalendar(user: user),
+          ScreenMenu(user: user),
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.star_rounded),
-            label: 'AI',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list),
-            label: 'Training',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today),
-            label: 'Calendar',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.menu),
-            label: 'Menu',
-          ),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.star_rounded), label: 'AI'),
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Training'),
+          BottomNavigationBarItem(icon: Icon(Icons.calendar_today), label: 'Calendar'),
+          BottomNavigationBarItem(icon: Icon(Icons.menu), label: 'Menu'),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.orange, // 선택된 아이템 색상 설정
-        unselectedItemColor: Colors.grey, // 선택되지 않은 아이템 색상 설정
+        selectedItemColor: Colors.orange,
+        unselectedItemColor: Colors.grey,
         onTap: _onItemTapped,
       ),
     );
--- knufit/lib/screen/screen_menu.dart
+++ knufit/lib/screen/screen_menu.dart
@@ -1,136 +1,211 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'profile_page.dart'; // ProfilePage를 import
+import '../screen/profile_page.dart';
 import '../auth/auth_helper.dart';
 import '../theme_notifier.dart';
-import '../database/db_helper.dart'; // DBHelper를 통해 데이터베이스 접근
-import 'dart:io';
 
-class ScreenMenu extends StatefulWidget {
-  final Map<String, dynamic> user; // 로그인 정보를 받을 변수
+class ScreenMenu extends StatelessWidget {
+  final Map<String, dynamic> user;
 
   const ScreenMenu({required this.user, Key? key}) : super(key: key);
 
   @override
-  _ScreenMenuState createState() => _ScreenMenuState();
-}
-
-class _ScreenMenuState extends State<ScreenMenu> {
-  late Map<String, dynamic> user;
-  final DBHelper _dbHelper = DBHelper(); // DBHelper 인스턴스 생성
-
-  @override
-  void initState() {
-    super.initState();
-    user = widget.user;
-
-    // 앱 시작 시 데이터베이스에서 프로필 이미지를 불러옴
-    _dbHelper.getProfileImage(user['id']).then((imagePath) {
-      if (imagePath != null) {
-        setState(() {
-          user['profile_image'] = imagePath;
-        });
-      }
-    });
-  }
-
-  // 메뉴 옵션
-  Widget _buildMenuOption(IconData icon, String title, VoidCallback onTap) {
-    return ListTile(
-      leading: Icon(icon, color: Colors.orange),
-      title: Text(title, style: TextStyle(color: Colors.orange)),
-      onTap: onTap,
-    );
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('메뉴'),
-        automaticallyImplyLeading: false, // 뒤로가기 버튼 제거
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 20),
-              Text(
-                '프로필',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 10),
-              GestureDetector(
-                onTap: () async {
-                  final updatedUser = await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ProfilePage(user: user), // 현재 유저 정보를 ProfilePage로 전달
-                    ),
-                  );
+    // Null 값을 기본값으로 처리
+    final String profileImg = user['profile_img'] ?? 'assets/profile_default.jpg';
+    final String userName = user['name'] ?? 'Unknown User';
+    final String userEmail = user['email'] ?? 'No Email';
 
-                  if (updatedUser != null) {
-                    setState(() {
-                      user = updatedUser; // 반환된 유저 정보로 업데이트
-                    });
-                  }
-                },
-                child: Container(
-                  padding: EdgeInsets.all(8.0), // 클릭 가능한 영역 확장
-                  decoration: BoxDecoration(
-                    color: Colors.transparent,
-                  ),
-                  child: Row(
-                    children: [
-                      CircleAvatar(
-                        radius: 40,
-                        backgroundColor: Colors.grey,
-                        backgroundImage: user['profile_image'] != null && File(user['profile_image']).existsSync()
-                            ? FileImage(File(user['profile_image'])) as ImageProvider
-                            : AssetImage('assets/profile_default.jpg'),
-                      ),
-                      SizedBox(width: 20),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            user['name'],
-                            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            user['email'],
-                            style: TextStyle(fontSize: 16, color: Colors.grey),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
+    return Scaffold(
+      appBar: AppBar(title: Text('메뉴')),
+      body: Column(
+        children: [
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ProfilePage(user: user),
                 ),
-              ),
-              SizedBox(height: 20),
-              Divider(color: Colors.black),
-              _buildMenuOption(Icons.logout, '로그아웃', () {
-                AuthHelper.confirmLogout(context);
-              }), // 로그아웃 메뉴
-              _buildMenuOption(Icons.password, '비밀번호 변경', () {
-                AuthHelper.changePassword(context, user);
-              }), // 비밀번호 변경
-              SwitchListTile(
-                title: Text("다크 모드", style: TextStyle(color: Colors.orange)),
-                value: Provider.of<ThemeNotifier>(context).isDarkMode,
-                onChanged: (value) {
-                  Provider.of<ThemeNotifier>(context, listen: false).toggleTheme();
-                },
-                secondary: Icon(Icons.brightness_6, color: Colors.orange),
-                activeColor: Colors.deepOrangeAccent,
-                activeTrackColor: Colors.orange,
-              ),
-            ],
+              );
+            },
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 40,
+                  backgroundImage: profileImg.startsWith('http')
+                      ? NetworkImage(profileImg)
+                      : AssetImage(profileImg) as ImageProvider,
+                ),
+                SizedBox(width: 20),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(userName, style: TextStyle(fontSize: 24)),
+                    Text(userEmail, style: TextStyle(color: Colors.grey)),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text('로그아웃'),
+            onTap: () => AuthHelper.confirmLogout(context),
+          ),
+          Divider(),
+          // 다크 모드 토글 기능 추가
+          SwitchListTile(
+            title: Text("다크 모드", style: TextStyle(color: Colors.orange)),
+            value: Provider.of<ThemeNotifier>(context).isDarkMode,
+            onChanged: (value) {
+              Provider.of<ThemeNotifier>(context, listen: false).toggleTheme();
+            },
+            secondary: Icon(Icons.brightness_6, color: Colors.orange),
+            activeColor: Colors.deepOrangeAccent,
+            activeTrackColor: Colors.orange,
+          ),
+        ],
       ),
     );
   }
 }
+
+
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// import 'profile_page.dart'; // ProfilePage를 import
+// import '../auth/auth_helper.dart';
+// import '../theme_notifier.dart';
+// import '../database/db_helper.dart'; // DBHelper를 통해 데이터베이스 접근
+// import 'dart:io';
+
+// class ScreenMenu extends StatefulWidget {
+//   final Map<String, dynamic> user; // 로그인 정보를 받을 변수
+
+//   const ScreenMenu({required this.user, Key? key}) : super(key: key);
+
+//   @override
+//   _ScreenMenuState createState() => _ScreenMenuState();
+// }
+
+// class _ScreenMenuState extends State<ScreenMenu> {
+//   late Map<String, dynamic> user;
+//   final DBHelper _dbHelper = DBHelper(); // DBHelper 인스턴스 생성
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     user = widget.user;
+
+//     // 앱 시작 시 데이터베이스에서 프로필 이미지를 불러옴
+//     _dbHelper.getProfileImage(user['id']).then((imagePath) {
+//       if (imagePath != null) {
+//         setState(() {
+//           user['profile_image'] = imagePath;
+//         });
+//       }
+//     });
+//   }
+
+//   // 메뉴 옵션
+//   Widget _buildMenuOption(IconData icon, String title, VoidCallback onTap) {
+//     return ListTile(
+//       leading: Icon(icon, color: Colors.orange),
+//       title: Text(title, style: TextStyle(color: Colors.orange)),
+//       onTap: onTap,
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('메뉴'),
+//         automaticallyImplyLeading: false, // 뒤로가기 버튼 제거
+//       ),
+//       body: SingleChildScrollView(
+//         child: Padding(
+//           padding: const EdgeInsets.all(16.0),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               SizedBox(height: 20),
+//               Text(
+//                 '프로필',
+//                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+//               ),
+//               SizedBox(height: 10),
+//               GestureDetector(
+//                 onTap: () async {
+//                   final updatedUser = await Navigator.push(
+//                     context,
+//                     MaterialPageRoute(
+//                       builder: (context) => ProfilePage(user: user), // 현재 유저 정보를 ProfilePage로 전달
+//                     ),
+//                   );
+
+//                   if (updatedUser != null) {
+//                     setState(() {
+//                       user = updatedUser; // 반환된 유저 정보로 업데이트
+//                     });
+//                   }
+//                 },
+//                 child: Container(
+//                   padding: EdgeInsets.all(8.0), // 클릭 가능한 영역 확장
+//                   decoration: BoxDecoration(
+//                     color: Colors.transparent,
+//                   ),
+//                   child: Row(
+//                     children: [
+//                       CircleAvatar(
+//                         radius: 40,
+//                         backgroundColor: Colors.grey,
+//                         backgroundImage: user['profile_image'] != null && File(user['profile_image']).existsSync()
+//                             ? FileImage(File(user['profile_image'])) as ImageProvider
+//                             : AssetImage('assets/profile_default.jpg'),
+//                       ),
+//                       SizedBox(width: 20),
+//                       Column(
+//                         crossAxisAlignment: CrossAxisAlignment.start,
+//                         children: [
+//                           Text(
+//                             user['name'],
+//                             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+//                           ),
+//                           Text(
+//                             user['email'],
+//                             style: TextStyle(fontSize: 16, color: Colors.grey),
+//                           ),
+//                         ],
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ),
+//               SizedBox(height: 20),
+//               Divider(color: Colors.black),
+//               _buildMenuOption(Icons.logout, '로그아웃', () {
+//                 AuthHelper.confirmLogout(context);
+//               }), // 로그아웃 메뉴
+//               // _buildMenuOption(Icons.password, '비밀번호 변경', () {
+//               //   AuthHelper.changePassword(context, user);
+//               // }), // 비밀번호 변경
+//               SwitchListTile(
+//                 title: Text("다크 모드", style: TextStyle(color: Colors.orange)),
+//                 value: Provider.of<ThemeNotifier>(context).isDarkMode,
+//                 onChanged: (value) {
+//                   Provider.of<ThemeNotifier>(context, listen: false).toggleTheme();
+//                 },
+//                 secondary: Icon(Icons.brightness_6, color: Colors.orange),
+//                 activeColor: Colors.deepOrangeAccent,
+//                 activeTrackColor: Colors.orange,
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
